55d64c28cefb0aca1c2b4482d108df160bdfc862
U201815353-AndreaCB <jeffthekiller3201@gmail.com>
2021-05-28 19:54:44 -0500

    HU_error

--- pickyourtutor/src/main/java/pe/edu/upc/spring/controller/PagoController.java
+++ pickyourtutor/src/main/java/pe/edu/upc/spring/controller/PagoController.java
@@ -11,15 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.sun.el.parser.ParseException;
-import pe.edu.upc.spring.model.Tutor;
-import pe.edu.upc.spring.service.ITutorService;
+import pe.edu.upc.spring.model.Pago;
+import pe.edu.upc.spring.service.IPagoService;
 
 @Controller
-@RequestMapping("/tutor")
+@RequestMapping("/pago")
 public class PagoController {
 
 	@Autowired
-	private ITutorService tService;
+	private IPagoService pService;
 
 	@RequestMapping("/bienvenido")
 	public String irPaginaBienvenida() {
@@ -27,41 +27,41 @@ public class PagoController {
 	}
 
 	@RequestMapping("/")
-	public String irPaginaListadoTutores(Map<String, Object> model) {
-		model.put("listaTutores", tService.listar());
-		return "listTutor";
+	public String irPaginaListadoPagos(Map<String, Object> model) {
+		model.put("listaPagos", pService.listar());
+		return "listPago";
 	}
 
 	@RequestMapping("/irRegistrar")
 	public String irPaginaRegistrar(Model model) {
-		model.addAttribute("tutor", new Tutor());
-		return "tutor";
+		model.addAttribute("pago", new Pago());
+		return "pago";
 	}
 
 	@RequestMapping("/registrar")
-	public String registrar(@ModelAttribute Tutor objTutor, BindingResult binRes, Model model) throws ParseException {
+	public String registrar(@ModelAttribute Pago objPago, BindingResult binRes, Model model) throws ParseException {
 		if (binRes.hasErrors())
-			return "tutor";
+			return "pago";
 		else {
-			boolean flag = tService.insertar(objTutor);
+			boolean flag = pService.insertar(objPago);
 			if (flag)
-				return "redirect:/tutor/listar";
+				return "redirect:/pago/listar";
 			else {
 				model.addAttribute("mensaje", "Ocurrió un error");
-				return "redirect:/tutor/irRegistrar";
+				return "redirect:/pago/irRegistrar";
 			}
 		}
 	}
 
 	@RequestMapping("/modificar/{id}")
 	public String modificar(@PathVariable int id, Model model, RedirectAttributes objRedir) throws ParseException {
-		Optional<Tutor> objTutor = tService.listarId(id);
-		if (objTutor == null) {
+		Optional<Pago> objPago = pService.listarId(id);
+		if (objPago == null) {
 			objRedir.addFlashAttribute("mensaje", "Ocurrió un error");
-			return "redirect:/tutor/listar";
+			return "redirect:/pago/listar";
 		} else {
-			model.addAttribute("tutor", objTutor);
-			return "tutor";
+			model.addAttribute("pago", objPago);
+			return "pago";
 		}
 	}
 	
@@ -69,20 +69,20 @@ public class PagoController {
 	public String eliminar(Map<String, Object> model, @RequestParam(value="id") Integer id) {
 		try {
 			if (id!=null && id>0) {
-				tService.eliminar(id);
-				model.put("listaTutores", tService.listar());
+				pService.eliminar(id);
+				model.put("listaPagos", pService.listar());
 			}
 		} catch (Exception ex) {
 			System.out.println(ex.getMessage());
 			model.put("mensaje", "Ocurrio un error");
-			model.put("listaTutor", tService.listar());
+			model.put("listaPago", pService.listar());
 		}
-		return "listTutor";
+		return "listPago";
 		}
 	
 	@RequestMapping("/listar")
 	public String listar(Map<String, Object> model) {
-		model.put("listaTutores", tService.listar());
-		return "listTutor";
+		model.put("listaPagos", pService.listar());
+		return "listPago";
 	}
 }
